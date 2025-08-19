7b39d321ea535933e0ca7ae3bb627b800ee3eda8
Sabith <masterdevsabith@gmail.com>
2025-08-19 22:25:52 +0530

    subdomain finds, updates to db folder, but very slow

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import requests
+
+
+website_url = input("enter target website url: ").strip().lower()
+
+
+def find_subdomain(url):
+    count = 0
+    with open('db/available_subdomain.txt', 'r+') as delete_this:
+        delete_this.truncate()
+    with open('data/subdomain_list.txt', 'r') as file:
+        for line in file:
+            try:
+                sub = line.strip()
+                x = requests.get(f"https://{sub}.{url}/", timeout=5)
+                if x.status_code == 200:
+                    with open('db/available_subdomain.txt', 'a') as file2:
+                        file2.write(line)
+                    count += 1
+                else:
+                    pass
+            except requests.exceptions.RequestException:
+                continue
+    return f'{count} subdomains found'
+
+
+result = find_subdomain(website_url)
+print(result)
+
+
+# geeksforgeeks.org
